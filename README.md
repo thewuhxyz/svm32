950d36bee9f1444172630e80361fa9f86be2421b
chizor iwuh <thisischizor@gmail.com>
2025-02-06 20:01:50 +0100

    run paytube

---
+++ README.md
@@ -0,0 +1,9 @@
+# SVM32
+
+**Q: Can I run the SVM on a 32-bit target?**
+
+A: ...
+
+#### Reference
+
+- [Paytube](https://github.com/buffalojoec/paytube)
---
+++ crates/paytube/src/lib.rs
@@ -0,0 +1,158 @@
+//! PayTube. A simple SPL payment channel.
+//!
+//! PayTube is an SVM-based payment channel that allows two parties to exchange
+//! tokens off-chain. The channel is opened by invoking the PayTube "VM",
+//! running on some arbitrary server(s). When transacting has concluded, the
+//! channel is closed by submitting the final payment ledger to Solana.
+//!
+//! The final ledger tracks debits and credits to all registered token accounts
+//! or system accounts (native SOL) during the lifetime of a channel. It is
+//! then used to to craft a batch of transactions to submit to the settlement
+//! chain (Solana).
+//!
+//! Users opt-in to using a PayTube channel by "registering" their token
+//! accounts to the channel. This is done by delegating a token account to the
+//! PayTube on-chain program on Solana. This delegation is temporary, and
+//! released immediately after channel settlement.
+//!
+//! Note: This opt-in solution is for demonstration purposes only.
+//!
+//! ```text
+//! 
+//! PayTube "VM"
+//!
+//!    Bob          Alice        Bob          Alice          Will
+//!     |             |           |             |             |
+//!     | --o--o--o-> |           | --o--o--o-> |             |
+//!     |             |           |             | --o--o--o-> | <--- PayTube
+//!     | <-o--o--o-- |           | <-o--o--o-- |             |    Transactions
+//!     |             |           |             |             |
+//!     | --o--o--o-> |           |     -----o--o--o----->    |
+//!     |             |           |                           |
+//!     | --o--o--o-> |           |     <----o--o--o------    |
+//!
+//!       \        /                  \         |         /
+//!
+//!         ------                           ------
+//!        Alice: x                         Alice: x
+//!        Bob:   x                         Bob:   x    <--- Solana Transaction
+//!                                         Will:  x         with final ledgers
+//!         ------                           ------
+//!
+//!           \\                               \\
+//!            x                                x
+//!
+//!         Solana                           Solana     <--- Settled to Solana
+//! ```
+//!
+//! The Solana SVM's `TransactionBatchProcessor` requires projects to provide a
+//! "loader" plugin, which implements the `TransactionProcessingCallback`
+//! interface.
+//!
+//! PayTube defines a `PayTubeAccountLoader` that implements the
+//! `TransactionProcessingCallback` interface, and provides it to the
+//! `TransactionBatchProcessor` to process PayTube transactions.
+
+mod loader;
+mod processor;
+mod settler;
+pub mod transaction;
+
+use {
+    crate::{
+        loader::PayTubeAccountLoader, settler::PayTubeSettler, transaction::PayTubeTransaction,
+    },
+    processor::{create_transaction_batch_processor, get_transaction_check_results},
+    solana_client::rpc_client::RpcClient,
+    solana_compute_budget::compute_budget::ComputeBudget,
+    solana_sdk::{
+        feature_set::FeatureSet, fee::FeeStructure, hash::Hash, rent_collector::RentCollector,
+        signature::Keypair,
+    },
+    solana_svm::transaction_processor::{
+        TransactionProcessingConfig, TransactionProcessingEnvironment,
+    },
+    std::sync::Arc,
+    transaction::create_svm_transactions,
+};
+
+/// A PayTube channel instance.
+///
+/// Facilitates native SOL or SPL token transfers amongst various channel
+/// participants, settling the final changes in balances to the base chain.
+pub struct PayTubeChannel {
+    /// I think you know why this is a bad idea...
+    keys: Vec<Keypair>,
+    rpc_client: RpcClient,
+}
+
+impl PayTubeChannel {
+    pub fn new(keys: Vec<Keypair>, rpc_client: RpcClient) -> Self {
+        Self { keys, rpc_client }
+    }
+
+    /// The PayTube API. Processes a batch of PayTube transactions.
+    ///
+    /// Obviously this is a very simple implementation, but one could imagine
+    /// a more complex service that employs custom functionality, such as:
+    ///
+    /// * Increased throughput for individual P2P transfers.
+    /// * Custom Solana transaction ordering (e.g. MEV).
+    ///
+    /// The general scaffold of the PayTube API would remain the same.
+    pub fn process_paytube_transfers(&self, transactions: &[PayTubeTransaction]) {
+        // PayTube default configs.
+        let compute_budget = ComputeBudget::default();
+        let feature_set = FeatureSet::all_enabled();
+        let fee_structure = FeeStructure::default();
+        let lamports_per_signature = fee_structure.lamports_per_signature;
+        let rent_collector = RentCollector::default();
+        
+        // PayTube loader/callback implementation.
+        let account_loader = PayTubeAccountLoader::new(&self.rpc_client);
+        
+        // Solana SVM transaction batch processor.
+        let processor =
+        create_transaction_batch_processor(&account_loader, &feature_set, &compute_budget);
+
+        println!("processor: {:#?}", &processor);
+        
+        
+        // The PayTube transaction processing runtime environment.
+        let processing_environment = TransactionProcessingEnvironment {
+            blockhash: Hash::default(),
+            epoch_total_stake: None,
+            epoch_vote_accounts: None,
+            feature_set: Arc::new(feature_set),
+            fee_structure: Some(&fee_structure),
+            lamports_per_signature,
+            rent_collector: Some(&rent_collector),
+        };
+        
+        // The PayTube transaction processing config for Solana SVM.
+        let processing_config = TransactionProcessingConfig {
+            compute_budget: Some(compute_budget),
+            ..Default::default()
+        };
+        
+        // 1. Convert to an SVM transaction batch.
+        let svm_transactions = create_svm_transactions(transactions);
+        
+        println!("here");
+        // 2. Process transactions with the SVM API.
+        let results = processor.load_and_execute_sanitized_transactions(
+            &account_loader,
+            &svm_transactions,
+            get_transaction_check_results(svm_transactions.len(), lamports_per_signature),
+            &processing_environment,
+            &processing_config,
+        );
+        
+        println!("here");
+        // 3. Convert results into a final ledger using a `PayTubeSettler`.
+        let settler = PayTubeSettler::new(&self.rpc_client);
+
+        // 4. Submit to the Solana base chain.
+        settler.process_settle(transactions, results, &self.keys);
+    }
+}
